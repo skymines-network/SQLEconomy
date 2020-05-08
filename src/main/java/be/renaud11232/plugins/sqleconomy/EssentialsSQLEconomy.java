3308627c60ab3e67b8bae18d63fb299b7bd38512
Renaud Gaspard <gaspardrenaud@hotmail.com>
2020-05-08 13:19:31 +0200

    not using AbstractEconomy anymore

--- src/main/java/be/renaud11232/plugins/sqleconomy/EssentialsSQLEconomy.java
+++ src/main/java/be/renaud11232/plugins/sqleconomy/EssentialsSQLEconomy.java
@@ -1,13 +1,15 @@
 package be.renaud11232.plugins.sqleconomy;
 
-import net.milkbowl.vault.economy.AbstractEconomy;
+import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.Plugin;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
 
-public class EssentialsSQLEconomy extends AbstractEconomy {
+public class EssentialsSQLEconomy implements Economy {
 
     private final String name = "SQL Economy";
     private final Logger logger;
@@ -20,7 +22,7 @@ public class EssentialsSQLEconomy extends AbstractEconomy {
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return plugin.isEnabled();
     }
 
     @Override
@@ -35,51 +37,81 @@ public class EssentialsSQLEconomy extends AbstractEconomy {
 
     @Override
     public int fractionalDigits() {
-        return 2;
+        return -1;
     }
 
     @Override
     public String format(double v) {
-        return null;
+        return v + " format";
     }
 
     @Override
     public String currencyNamePlural() {
-        return null;
+        return "plur";
     }
 
     @Override
     public String currencyNameSingular() {
-        return null;
+        return "sing";
     }
 
     @Override
     public boolean hasAccount(String s) {
+        return true;
+    }
+
+    @Override
+    public boolean hasAccount(OfflinePlayer offlinePlayer) {
         return false;
     }
 
     @Override
     public boolean hasAccount(String s, String s1) {
+        return true;
+    }
+
+    @Override
+    public boolean hasAccount(OfflinePlayer offlinePlayer, String s) {
         return false;
     }
 
     @Override
     public double getBalance(String s) {
+        return 20;
+    }
+
+    @Override
+    public double getBalance(OfflinePlayer offlinePlayer) {
         return 0;
     }
 
     @Override
     public double getBalance(String s, String s1) {
+        return 20;
+    }
+
+    @Override
+    public double getBalance(OfflinePlayer offlinePlayer, String s) {
         return 0;
     }
 
     @Override
     public boolean has(String s, double v) {
+        return true;
+    }
+
+    @Override
+    public boolean has(OfflinePlayer offlinePlayer, double v) {
         return false;
     }
 
     @Override
     public boolean has(String s, String s1, double v) {
+        return true;
+    }
+
+    @Override
+    public boolean has(OfflinePlayer offlinePlayer, String s, double v) {
         return false;
     }
 
@@ -89,7 +121,17 @@ public class EssentialsSQLEconomy extends AbstractEconomy {
     }
 
     @Override
+    public EconomyResponse withdrawPlayer(OfflinePlayer offlinePlayer, double v) {
+        return null;
+    }
+
+    @Override
     public EconomyResponse withdrawPlayer(String s, String s1, double v) {
+        return null;
+    }
+
+    @Override
+    public EconomyResponse withdrawPlayer(OfflinePlayer offlinePlayer, String s, double v) {
         return null;
     }
 
@@ -99,62 +141,97 @@ public class EssentialsSQLEconomy extends AbstractEconomy {
     }
 
     @Override
+    public EconomyResponse depositPlayer(OfflinePlayer offlinePlayer, double v) {
+        return null;
+    }
+
+    @Override
     public EconomyResponse depositPlayer(String s, String s1, double v) {
         return null;
     }
 
     @Override
-    public EconomyResponse createBank(String s, String s1) {
+    public EconomyResponse depositPlayer(OfflinePlayer offlinePlayer, String s, double v) {
         return null;
     }
 
     @Override
-    public EconomyResponse deleteBank(String s) {
+    public EconomyResponse createBank(String name, String playerName) {
+        return new EconomyResponse(0, 0, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "SQLEconomy does not support bank accounts");
+    }
+
+    @Override
+    public EconomyResponse createBank(String s, OfflinePlayer offlinePlayer) {
         return null;
     }
 
     @Override
-    public EconomyResponse bankBalance(String s) {
+    public EconomyResponse deleteBank(String name) {
+        return new EconomyResponse(0, 0, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "SQLEconomy does not support bank accounts");
+    }
+
+    @Override
+    public EconomyResponse bankBalance(String name) {
+        return new EconomyResponse(0, 0, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "SQLEconomy does not support bank accounts");
+    }
+
+    @Override
+    public EconomyResponse bankHas(String name, double amount) {
+        return new EconomyResponse(0, 0, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "SQLEconomy does not support bank accounts");
+    }
+
+    @Override
+    public EconomyResponse bankWithdraw(String name, double amount) {
+        return new EconomyResponse(0, 0, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "SQLEconomy does not support bank accounts");
+    }
+
+    @Override
+    public EconomyResponse bankDeposit(String name, double amount) {
+        return new EconomyResponse(0, 0, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "SQLEconomy does not support bank accounts");
+    }
+
+    @Override
+    public EconomyResponse isBankOwner(String name, String playerName) {
+        return new EconomyResponse(0, 0, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "SQLEconomy does not support bank accounts");
+    }
+
+    @Override
+    public EconomyResponse isBankOwner(String s, OfflinePlayer offlinePlayer) {
         return null;
     }
 
     @Override
-    public EconomyResponse bankHas(String s, double v) {
-        return null;
+    public EconomyResponse isBankMember(String name, String playerName) {
+        return new EconomyResponse(0, 0, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "SQLEconomy does not support bank accounts");
     }
 
     @Override
-    public EconomyResponse bankWithdraw(String s, double v) {
-        return null;
-    }
-
-    @Override
-    public EconomyResponse bankDeposit(String s, double v) {
-        return null;
-    }
-
-    @Override
-    public EconomyResponse isBankOwner(String s, String s1) {
-        return null;
-    }
-
-    @Override
-    public EconomyResponse isBankMember(String s, String s1) {
+    public EconomyResponse isBankMember(String s, OfflinePlayer offlinePlayer) {
         return null;
     }
 
     @Override
     public List<String> getBanks() {
-        return null;
+        return Collections.emptyList();
     }
 
     @Override
     public boolean createPlayerAccount(String s) {
+        return true;
+    }
+
+    @Override
+    public boolean createPlayerAccount(OfflinePlayer offlinePlayer) {
         return false;
     }
 
     @Override
     public boolean createPlayerAccount(String s, String s1) {
+        return true;
+    }
+
+    @Override
+    public boolean createPlayerAccount(OfflinePlayer offlinePlayer, String s) {
         return false;
     }
 }
